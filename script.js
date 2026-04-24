a2b89ae6598cc9e52b26309697944e559601314d
Anvar Obidov <varan193@mail.ru>
2026-04-24 08:44:47 +0300

    test

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ result.forEach(day => console.log(day));*/
 — В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
 
 — Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
-*/
+
 
 let myStr = "моя строка";
 let myNum = 12;
@@ -112,4 +112,5 @@ function checkString(input) {
 console.log(checkString(myStr))
 console.log(checkString(myNum))
 console.log(checkString(myStr2))
-
+*/
+console.log('test')
