78a06e2f3388d0affbb91d419cf34b85b0edd11d
Anvar Obidov <varan193@mail.ru>
2026-04-29 15:57:38 +0300

    Урок 9 готов

--- script.js
+++ script.js
@@ -1,40 +1,66 @@
-// 'use strict';
-// Необходимо выполнить в отдельном js файле, подключенному к отдельной HTML странице
-// 1) Создать массив week и записать в него дни недели в виде строк
-// Вывести на экран все дни недели
-// Каждый из них с новой строчки
-// Выходные дни - курсивом
-// Текущий день - жирным шрифтом(использовать объект даты)
-
 const week = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+  "Воскресенье",
 ];
-let curDay = "";
-curDay = new Date().getDay();
 
-for (let key in week) {
-  let day = week[key];
-  console.log(day);
+const month = [
+"Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"
+];
 
-  if (key == curDay - 1) {
-    const div = document.createElement("div");
-    day = `<b>${week[key]}</b>`;
-  } else {const div = document.createElement("div");
-    day = week[key];};
-  
-  if (key == 5 || key == 6) {
-    const div = document.createElement("div");
-    day = `<i>${day}</i>`;
-  } else {const div = document.createElement("div");
-    day = week[key]};
-  
-  const div = document.createElement("div");
-    div.innerHTML = day;
-    document.body.appendChild(div);
+const wordsHour = ["час", "часа", "часов"];
+const wordsMinutes = ["минута", "минуты", "минут"];
+const wordsSecondes = ["секунда", "секунды", "секунд"];
+
+function declensionNum(num, words) {    
+    const lastDigit = num % 10;
+    const lastTwoDigits = num % 100;
+    
+    if (lastTwoDigits > 10 && lastTwoDigits < 20) {
+        return words[2];
+    }
+    if (lastDigit > 1 && lastDigit < 5) {
+        return words[1];
+    }
+    if (lastDigit === 1) {
+        return words[0];
+    }
+    return words[2];
+}
+
+function formatNumber(num) {
+    return num < 10 ? '0' + num : num;
+}
+
+function updateDateTime() {
+    const now = new Date();
+    let curDay = now.getDay();
+    let year = now.getFullYear();
+    let monthOfTheYear = now.getMonth();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let dayOfTheWeek = week[curDay === 0 ? 6 : curDay-1];
+    let dayOfMonth = now.getDate();
+
+    const dateFormatA = `Сегодня ${dayOfTheWeek}, ${dayOfMonth} ${month[monthOfTheYear]} ${year} года, ${hour} ${declensionNum(hour, wordsHour)} ${minutes} ${declensionNum(minutes, wordsMinutes)} ${seconds} ${declensionNum(seconds, wordsSecondes)}`;
+    const dateFormatB = `${formatNumber(dayOfMonth)}.${formatNumber(monthOfTheYear + 1)}.${year} - ${formatNumber(hour)}:${formatNumber(minutes)}:${formatNumber(seconds)}`;
+
+
+    return `${dateFormatA}<br>${dateFormatB}`;
   }
+
+
+const div = document.createElement("div");
+document.body.appendChild(div);
+
+function refresh() {
+    div.innerHTML = updateDateTime();
+}
+
+refresh();
+setInterval(refresh, 1000);
